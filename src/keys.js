8fc0770e714ad3b7de9d120706bf972450e55baa
droboc <66892700+droboc@users.noreply.github.com>
2022-01-17 23:37:06 -0500

    clone

---
+++ src/keys.js
@@ -0,0 +1,8 @@
+module.exports={
+    database:{
+        host: "us-cdbr-east-05.cleardb.net",
+        user:'b0f0d268d80fe2',
+        password: 'e9d8e56a',
+        database:'heroku_8a7b5b49f98334b',
+    }
+};
